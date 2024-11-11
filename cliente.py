b83a05b236d21a0c6dcd3a04b8b86ed5e2d5e2a4
Andrey <jhonyandreyburga@gmail.com>
2024-11-11 15:54:33 -0500

    Actualizar mensajes de consola en el cliente para operaciones de suma, resta, multiplicación y división

--- cliente.py
+++ cliente.py
@@ -1,18 +1,22 @@
 import grpc
 import calculator_pb2
 import calculator_pb2_grpc
-
+ 
 def run():
       with grpc.insecure_channel('3.17.172.30:50051') as channel:
+
         stub = calculator_pb2_grpc.CalculatorStub(channel)
         response = stub.Add(calculator_pb2.OperationRequest(a=10, b=5))
-        print(f'Add: {response.result}')
+        print(f'La suma de los numeros es : {response.result}')
+
         response = stub.Subtract(calculator_pb2.OperationRequest(a=10, b=5))
-        print(f'Subtract: {response.result}')
+        print(f'La resta de los numeros es: {response.result}')
+
         response = stub.Multiply(calculator_pb2.OperationRequest(a=10, b=5))
-        print(f'Multiply: {response.result}')
-        response = stub.Divide(calculator_pb2.OperationRequest(a=10, b=5))
-        print(f'Divide: {response.result}')
+        print(f'La multiplicacion de los numeros es: {response.result}')
+
+        response = stub.Divide(calculator_pb2.OperationRequest(a=10, b=1))
+        print(f'La divicion de los numeros es: {response.result}')
 
 if __name__ == '__main__':
     run()
